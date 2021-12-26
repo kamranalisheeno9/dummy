3e7ef8344f1e648a6ce25047f06c1dde9240a052
unknown <kamranalisheeno9@gmail.com>
2021-12-26 11:05:23 +0500

    dummy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data,setData]=useState("")
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'React POST Request Example' })
+  };
+
+    fetch('http://localhost:3000/api/markets',requestOptions)
+        .then(response => response.json())
+        .then(data => setData(data));
+        console.log(data)
+
+}, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {data.spread}
+      </p>
     </div>
   );
 }
